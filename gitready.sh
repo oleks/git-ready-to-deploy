e3b26b5b15006e04b56615cb7841cb6596d794a4
oleks <oleks@oleks.info>
2016-11-11 09:40:13 +0100

    Initial commit

---
+++ gitready.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+failcode=2
+
+nstatuslines=$(git status . --porcelain | wc -l)
+if [ $nstatuslines -ne 0 ] ; then
+  cat <<EOF
+#####################################################
+# Nice! You have changes. Now commit or stash them. #
+#####################################################
+EOF
+  git status .
+  exit ${failcode}
+fi
+
+echo "Let me check your remote.."
+git remote update
+
+gitstatus=$(git status .)
+set +e
+echo "${gitstatus}" | grep "Your branch is up-to-date" >/dev/null
+code=$?
+set -e
+
+if [ $code -ne 0 ] ; then
+  cat <<EOF
+#####################################
+# Nice! You have commits. Now push. #
+#####################################
+EOF
+  git status .
+  exit ${failcode}
+fi
+
+echo "Looks like you are up-to-date."
